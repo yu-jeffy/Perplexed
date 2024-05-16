0dbe7435d1a6fe57da11274dc4ad614a5f17c253
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2024-05-16 16:17:58 +0000

    next.js scaffold

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../styles/page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        
+      </main>
+    </div>
+  );
+}
